06d928b2b0c1c2d8c43f7d0c3ec8b3a81532435c
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-06-12 18:24:21 -0500

    Action Section change suggestions

--- actions/control_variable.js
+++ actions/control_variable.js
@@ -13,7 +13,7 @@ module.exports = {
 	// This is the section the action will fall into.
 	//---------------------------------------------------------------------
 
-	section: "Other Stuff",
+	section: "Variable Things",
 
 	//---------------------------------------------------------------------
 	// Action Subtitle
